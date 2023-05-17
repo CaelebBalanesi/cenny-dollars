e2b727af780add4517e6db213f8c94f6fa5a2a73
Caeleb Balanesi <calinux22@gmail.com>
2023-05-16 22:12:56 -0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FirebaseService } from './firebase.service';
+import { Observable } from 'rxjs';
+import { MatDialogRef, MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { User } from './User';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'cenny-dollars';
+
+  constructor(private firebaseService: FirebaseService, public dialog: MatDialog) {
+    this.users$ = firebaseService.getUsers()
+  }
+
+  users$: Observable<User[]>;
+  username: string = "Unknown User";
+  recipient!: string;
+  money: number = 0;
+  amount!: number;
+
+  getMoney() {
+    this.firebaseService.getMoneyByUsername(this.username)
+      .subscribe(amount => this.money = amount);
+  }
+
+  sendMoney() {
+    this.firebaseService.sendMoney(this.username, this.recipient, this.amount)
+      .then(() => {
+        console.log('Money sent successfully');
+      })
+      .catch(error => {
+        console.error('Failed to send money:', error);
+      });
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(LoginDialog);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(!result) return;
+      console.log(result);
+      this.username = result;
+      this.getMoney();
+    });
+  }
+}
+
+@Component({
+  selector: 'dialog-animations-example-dialog',
+  template: `
+    <div class="container">
+      Username: <input [(ngModel)]="username" placeholder="username">
+      <div mat-dialog-actions>
+        <button mat-button [mat-dialog-close]="username">submit</button>
+        <button (click)="onNoClick()">close</button>
+      </div>
+    </div>
+  `,
+  styles: [
+    ".container {height: 200px; width: 500px; top: 0px; left: 0px;}",
+  ]
+})
+export class LoginDialog {
+  constructor(public dialogRef: MatDialogRef<LoginDialog>, @Inject(MAT_DIALOG_DATA) public data: any) {}
+
+  username!: string;
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { firebaseConfig } from './firebase-config';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import { LoginDialog } from './app.component';
+import {MatInputModule} from '@angular/material/input';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginDialog
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatInputModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
